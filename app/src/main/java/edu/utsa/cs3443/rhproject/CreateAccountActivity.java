a88e7d2b74a1eabcc1bb5d522f7b6d2bcf50b877
Steven Kalapaca <141975150+skalapaca@users.noreply.github.com>
2024-02-24 23:44:06 -0600

    added functionality to submit and reset buttons

--- app/src/main/java/edu/utsa/cs3443/rhproject/CreateAccountActivity.java
+++ app/src/main/java/edu/utsa/cs3443/rhproject/CreateAccountActivity.java
@@ -1,2 +1,56 @@
-package edu.utsa.cs3443.rhproject;public class CreateAccountActivity {
+package edu.utsa.cs3443.rhproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CreateAccountActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+
+        TextView newUsername = (EditText) findViewById(R.id.editTextTextNewUsername);
+        TextView newPassword = (EditText)findViewById(R.id.editTextTextNewPassword);
+        TextView confirmNewPassword = (EditText)findViewById(R.id.editTextTextConfirmNewPassword);
+        Button submitButton = findViewById(R.id.submit_button);
+        Button resetButton = findViewById(R.id.reset_button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = newUsername.getText().toString();
+                String npass = newPassword.getText().toString();
+                String cnpass = confirmNewPassword.getText().toString();
+                Toast toast;
+                if ( (!user.equals("")) && (npass.equals(cnpass)) ) {
+                    //create new Account object using user and npass
+                    launchActivity();
+                }
+                else {
+                    toast = Toast.makeText(view.getContext(), "Invalid user data!", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newUsername.setText("");
+                newPassword.setText("");
+                confirmNewPassword.setText("");
+            }
+        });
+    }
+    private void launchActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/edu/utsa/cs3443/rhproject/LoginActivity.java
+++ app/src/main/java/edu/utsa/cs3443/rhproject/LoginActivity.java
@@ -1,13 +1,41 @@
 package edu.utsa.cs3443.rhproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.login_button);
+        TextView createAccount = (TextView)findViewById(R.id.create_account);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Nice one!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+    }
+    private void launchActivity() {
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
